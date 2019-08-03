9b138a9a6700420271ba91a7592f6fc68b979b4b
Eneida Sanchez <eneida.sanchez@unosquare.com>
2019-08-03 13:55:04 -0500

    test

---
+++ Exercise3.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+
+namespace Exercise3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
